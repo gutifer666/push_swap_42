4a16435458fe215cf0de88e6484a4149f1b88cc3
Francisco Javier Gutierrez Perez <frgutier@c3r4s6.42malaga.com>
2023-03-02 10:54:24 +0100

    deleted stdio lib

--- push_swap.h
+++ push_swap.h
@@ -1,5 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frgutier <frgutier@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/02 09:37:20 by frgutier          #+#    #+#             */
+/*   Updated: 2023/03/02 09:42:30 by frgutier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 # include	"libft/libft.h"
-# include <stdio.h>
+# include   <stdio.h>
 #endif
