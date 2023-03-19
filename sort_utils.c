cb3b2a04efa7eb8c0902a3cd96ab2720530b9f7d
Francisco Javier Gutierrez Perez <frgutier@c3r4s6.42malaga.com>
2023-03-19 13:55:45 +0100

    Norminette ok

--- sort_utils.c
+++ sort_utils.c
@@ -6,7 +6,7 @@
 /*   By: frgutier <frgutier@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 09:14:38 by frgutier          #+#    #+#             */
-/*   Updated: 2023/03/13 08:48:57 by frgutier         ###   ########.fr       */
+/*   Updated: 2023/03/19 13:52:52 by frgutier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,26 +37,26 @@ int	is_order(t_list **stack, int size)
 	int	i;
 
 	i = 0;
-	while ((*stack)->index != 0) // Este bucle es para que el primer elemento sea 0
+	while ((*stack)->index != 0)
 	{
 		rotate(stack);
-		i++; // i es el número de veces que se ha rotado la lista
+		i++;
 	}
-	if (is_sorted(stack) == 1) // Comprueba si la lista está ordenada
+	if (is_sorted(stack) == 1)
 	{
-		if (i > (size / 2)) // Si i es mayor que la mitad de la lista, se rota al revés
+		if (i > (size / 2))
 		{
-			if (size == 5 && i == 3) // Si la lista tiene 5 elementos y se ha rotado 3 veces, se rota 2 veces al revés porque es más eficiente que rotar 3 veces, es un caso especial
+			if (size == 5 && i == 3)
 				ft_printf("rra\n");
 			ft_printf("rra\n");
 		}
-		else // Si no, se rota normalmente
-			while (i-- > 0) // Se rota tantas veces como i
+		else
+			while (i-- > 0)
 				ft_printf("ra\n");
-		return (1); // Devuelve 1 si la lista está ordenada
+		return (1);
 	}
-	else // Si la lista no está ordenada, se rota al revés tantas veces como i
-		while (i-- > 0) 
+	else
+		while (i-- > 0)
 			reverse_rotate(stack);
-	return (0); // Devuelve 0 si la lista no está ordenada
+	return (0);
 }
